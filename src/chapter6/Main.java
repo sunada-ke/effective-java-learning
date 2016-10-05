56b17a4d4e68a95c497a9613c11060d67db10af7
sunada-ke <sunada-ke@legendapl.com>
2016-10-05 13:50:59 +0900

    [UPDATE] Add program for WeakHashMap

--- src/chapter6/Main.java
+++ src/chapter6/Main.java
@@ -1,5 +1,8 @@
 package chapter6;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
 /*
  * 廃れたオブジェクト参照を取り除く
  */
@@ -11,6 +14,20 @@ public final class Main {
 		Stack stack = new Stack();
 		stack.push(new Object());
 		stack.pop();
+
+		// オブジェクト参照を一旦キャッシュに保持すると、オブジェクトへの参照がそこにあることを忘れがちになる。
+		// そこで、弱い参照をもつ WeakHashMap で保持することが推奨されている。
+		// WeakHashMap はエントリのキーが弱い参照で保持されており、そのキーが外部から参照されなくなると、GC の対象となる。
+		// コールバックやリスナーも同様で、クライアントが削除を忘れた場合にはリークしてしまうため、同じく WeakHashMap で保持するべき。
+		Integer key = new Integer(1);
+		Map<Integer, String> cache = new WeakHashMap<Integer, String>();
+		cache.put(key, "entry");
+		// エントリーに対するキー値の参照に null を設定。
+		key = null;
+		// 明示的に GC。
+		System.gc();
+		// キーが外部から参照されていないため、GC の対象となる。
+		System.out.println(cache.get(new Integer(1)));
 	}
 
 }
