d3e7b66a3a9a30ce3fdec52d202aa863a9f045fc
sunada-ke <sunada-ke@legendapl.com>
2016-10-20 18:28:19 +0900

    Test Commit

--- src/chapter13/Constants.java
+++ src/chapter13/Constants.java
@@ -7,7 +7,7 @@ import java.util.List;
 public final class Constants {
 
 	// 長さが 0 ではない配列は常に可変であることを意識する必要がある。
-	// final フィールドの変数は参照は変更できないが、内部のデータ更できてしまう。
+	// final フィールドの変数は参照は変更できないが、内部のデータ変更できてしまう。
 	// public static final の配列フィールドやそのような配列を返すアクセッサーを持つのはほとんど誤り。
 	// 変更不可能な配列として他クラスに返す必要がある。
 	private static final String[] PRIVATE_STRING_VALUES = { "a", "b", "c" };
